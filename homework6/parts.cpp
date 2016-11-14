ae8380307dc9f233e97ee8bd647fe5c87c4ff3a5
abhi6 <abhi6@LAPTOP-VAUO1DC4>
2016-11-13 23:26:36 -0600

    Add project files.

---
+++ homework6/parts.cpp
@@ -0,0 +1,36 @@
+#include "parts.h"
+
+void Torso::mysetter(string newname, int p_num, string type, double weight, double cost, string desc)
+{}
+//void robo::setter(string Name, int P_num, string Type, double Weight, double Cost, string Desc) {
+//name = Name; p_num = P_num; type = Type; weight = Weight; cost = Cost; desc = Desc;
+//}
+
+void Battery::mysetter(string name, int p_num, string type, double weight, double cost, string desc)
+{
+}
+
+void Battery::setBattery(double Energy, double power)
+{
+	energy = Energy; maxPower = power;
+}
+
+void Arm::mysetter(string name, int p_num, string type, double weight, double cost, string desc)
+{
+}
+
+void Arm::setArm(int Power_Con) { power_con = Power_Con; }
+
+/*Head::Head() { type = "Head"; };
+Torso::Torso() { type = "Torso"; };
+Arm::Arm() { type = "Arm"; };
+Locomotor::Locomotor() { type = "Locomotor"; };
+Battery::Battery() { type = "Battery"; };*/
+
+void Head::mysetter(string name, int p_num, string type, double weight, double cost, string desc)
+{
+}
+
+void Locomotor::mysetter(string name, int p_num, string type, double weight, double cost, string desc)
+{
+}
---
+++ homework6/parts.h
@@ -0,0 +1,80 @@
+#ifndef PARTS_H
+#define PARTS_H
+#include <string>
+#include <vector>
+#include <fstream>
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Menu_Bar.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Multiline_Input.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Return_Button.H>
+#include <iostream>
+#include <string>
+#include<iterator>
+#include"parts.h"
+using namespace std;
+
+
+class robo
+{
+public:
+	string name;
+	int p_num;
+	string type;
+	double weight;
+	double cost;
+	string desc;
+	//robo();
+	//void setter();
+	virtual void mysetter(string name, int p_num, string type, double weight, double cost, string desc) = 0;
+};
+
+class Locomotor : public robo
+{
+public:
+	//Locomotor();
+	virtual void mysetter(string name, int p_num, string type, double weight, double cost, string desc);
+	int max_speed;
+	int power_conloco;
+};
+
+class Arm : public robo
+{
+public:
+	//Arm();
+	virtual void mysetter(string name, int p_num, string type, double weight, double cost, string desc);
+	void setArm(int Power_Con);
+	int power_con;
+};
+
+class Battery : public robo
+{
+public:
+	virtual void mysetter(string name, int p_num, string type, double weight, double cost, string desc);
+	//Battery();
+	void setBattery(double Energy, double power);
+	double energy;
+	double maxPower;
+};
+
+class Head : public robo
+{
+public:
+	//Head();
+	virtual void mysetter(string name, int p_num, string type, double weight, double cost, string desc);
+};
+
+class Torso : public robo
+{
+public:
+	//Torso();
+	virtual void mysetter(string name, int p_num, string type, double weight, double cost, string desc);
+	Head tHead;
+	vector<Arm> tArm;
+	int battery_compartments;
+	Locomotor tLoco;
+	vector<Battery> tBattery;
+};
+#endif
