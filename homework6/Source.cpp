b3587f5a3202d00a9f73fc89726140467f9c1b4a
Muhammad Siddiqui <muhammadssiddiqui@mavs.uta.edu>
2016-11-22 04:21:23 -0600

    final

--- homework6/Source.cpp
+++ homework6/Source.cpp
@@ -14,22 +14,20 @@ void createCB(Fl_Widget*w, void* p)
 	win1->hide();
 	create();
 }
-void viewCB(Fl_Widget*w, void* p)
-{
-	win1->hide();
-	//cm();
-
-}
+//void viewCB(Fl_Widget*w, void* p)
+//{
+//	win1->hide();
+//}
 
 int main()
 {
 	Fl::scheme("gtk+");
 	win1 = new Fl_Window{ 600, 400, "Robbie Robot Shop" };
 	Fl_Button create(200, 50, 200, 30, "Create");
-	Fl_Button view(200, 100, 200, 30, "View");
+	Fl_Button view(200, 100, 200, 30, "View*");
 
 	create.callback(createCB);
-	view.callback(viewCB);
+	//view.callback(viewCB);
 	win1->end();
 	win1->show();
 
